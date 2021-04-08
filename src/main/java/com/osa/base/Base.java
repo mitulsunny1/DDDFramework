772a5855cf38dac4c91cde1d60b6f8ebd73e3c1b
Md Obaidulla <mitulsunny8@gmail.com>
2021-04-07 21:49:55 -0400

    added regression and sanity suite

--- src/main/java/com/osa/base/Base.java
+++ src/main/java/com/osa/base/Base.java
@@ -3,8 +3,11 @@ package com.osa.base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 public class Base {
@@ -13,6 +16,14 @@ public WebDriver dr;
 //public void beforeClass() {
 //	dr= Browser.openBrowser("firefox");
 //}
+@BeforeSuite
+public void beforeSuite() {
+	System.out.println("===========The test is running on "+System.getProperty("os.name")+"==========");
+}
+@AfterSuite
+public void afterSuite() {
+	System.out.println("********All the Test is executed and send the reports to the email**********");
+}
 @BeforeMethod
 @Parameters("browser")
 public void beforeMethod(String browser) {
